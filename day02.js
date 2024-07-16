ef8e2081dec6c53286a5793fa723af04ac8bf540
Aryan <aryanbaghel040@gmail.com>
2024-07-16 19:28:12 +0530

    first commit

---
+++ day02.js
@@ -0,0 +1,55 @@
+let a = 1;
+let b = 2;
+
+// task 1
+console.log(a + b);
+
+// task 2
+console.log(a - b);
+
+// task 3
+console.log(a * b);
+
+// task 4
+console.log(a / b);
+
+// task 5
+console.log(a % b);
+
+// task 6
+a += 10;
+console.log(a);
+
+// task 7
+a -= 5;
+console.log(a);
+
+// task 8
+console.log(a > b);
+console.log(a < b);
+
+// task 9
+console.log(a <= b);
+console.log(a >= b);
+
+// task 10
+console.log(a == b);
+console.log(a === b);
+
+// Task 11
+if (a >= 5 && b >= 0) {
+  console.log("true");
+}
+
+// Task 12
+if (a >= 5 || b >= 0) {
+  console.log("true");
+}
+
+// Task 13
+if (a != b) {
+  console.log("true");
+}
+
+// Task 14
+Math.sign(-a) === 1 ? console.log("Positive") : console.log("Negative");
